19dd9a62f08b6b5e7c91b28eb3c3227f5145fbef
mystickoder <89115482+mystickoder@users.noreply.github.com>
2022-02-22 08:58:16 -0500

    Add files via upload

---
+++ pizza.py
@@ -0,0 +1,21 @@
+requested_toppings = ['mushrooms', 'onions', 'olives', 'extra cheese']
+odd_toppings = ['french fries', 'nochos']
+
+if "french fries" in requested_toppings:
+    print('add fries')
+    
+else:
+    print("no fries added")
+if 'hamburger' in requested_toppings:
+    print('add extra meat')
+    
+if 'mushrooms' in requested_toppings:
+    print('add onions')
+    
+else:
+    print('no shrooms')
+if 'olives' 'nochos' in requested_toppings:
+    print('we are going to have a party')
+    
+else:
+    print('your order is ready')
